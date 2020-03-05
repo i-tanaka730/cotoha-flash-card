eb0d9f7345dcdd00b46d4e0ba8ccd83c8a7d3678
i-tanaka730 <pttp730@gmail.com>
2020-03-05 23:53:18 +0900

    upd

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="container">
-  <b-card tag="article" style="width: 40rem;" class="mb-2">
-    <b-button href="#" variant="primary" v-on:click="showAnswer">答え</b-button>
-    <b-button href="#" variant="primary" v-on:click="updateQuestion">次の問題</b-button>
-    <b-card-text>{{ question }}</b-card-text>
-    <b-card-text>{{ answer }}</b-card-text>
-  </b-card>
+    <b-card tag="article" style="width: 30rem;" class="mb-2">
+      <h2><b-badge variant="success">COTOHA FLASH CARD</b-badge></h2>
+      <br/>
+      <b-card-text>{{ question }}</b-card-text>
+      <br/>
+      <b-dropdown variant="success" id="dropdown-1" text="答え" class="m-md-2">
+        <b-dropdown-item>{{ answer }}</b-dropdown-item>
+      </b-dropdown>
+      <b-button variant="success" v-on:click="nextQuestion">次の問題</b-button>
+    </b-card>
   </div>
 </template>
 
@@ -13,9 +17,7 @@
 import axios from 'axios'
 import Enumerable from 'linq';
 
-const DEVELOPER_API_BASE_URL = "https://api.ce-cotoha.com/api/dev/nlp/"
-const DEVELOPER_API_PARSE_URL = DEVELOPER_API_BASE_URL + "v1/parse"
-const DEVELOPER_API_KEYWORD_URL = DEVELOPER_API_BASE_URL + "v1/keyword"
+const DEVELOPER_API_KEYWORD_URL = "https://api.ce-cotoha.com/api/dev/nlp/v1/keyword"
 const ACCESS_TOKEN_PUBLISH_URL = "https://api.ce-cotoha.com/v1/oauth/accesstokens"
 const MICRO_CMS_QUESTION_URL = "https://cotoha-flash-card.microcms.io/api/v1/question"
 
@@ -25,54 +27,55 @@ const MICRO_CMS_API_KEY = ""
 
 export default {
 
-  data () {
+  async asyncData () {
+    const questions = await getAllQuestions()
+    const question = await getQuestionAtRandom(questions)
+    const answer = await getKeyword(question)
+    const emoticonQuestion = await convertEmoticonQuestion(question, answer)
+
     return {
-      question: "",
-      answer: ""
+      questions : questions,
+      question : emoticonQuestion,
+      answer : answer
     }
   },
 
   methods: {
-    showAnswer: function() {
-      this.answer = "adsfafd"
-    }
-  },
+    nextQuestion: async function() {
+      const question = await getQuestionAtRandom(this.questions)
+      const answer = await getKeyword(question)
+      const emoticonQuestion = await convertEmoticonQuestion(question, answer)
 
-async asyncData () {
-
-    try {
-      const questions = await getAllQuestions()
-      const question = await getQuestion(questions)
-      const token = await getAccessToken()
-      const keyword = await getKeyword(question, token)
-
-      return {
-        question : question,
-        answer : keyword
-      }
-
-    } catch (err) {
-      console.log("==========例外発生==========")
-      console.log(err)
+      this.question = emoticonQuestion
+      this.answer = answer
     }
   }
 }
 
 async function getAllQuestions()
 {
-  const questionResult = await axios.get(MICRO_CMS_QUESTION_URL, {
-    headers: { "X-API-KEY": MICRO_CMS_API_KEY }
-  })
+  const apiHeaders = {
+    headers:{
+      "X-API-KEY": MICRO_CMS_API_KEY
+    }
+  }
 
-  const questions = Enumerable.from(questionResult.data.contents).select(c => c.question).toArray()
+  const questionResult = await axios.get(MICRO_CMS_QUESTION_URL, apiHeaders)
+  const contents = Enumerable.from(questionResult.data.contents)
+  const questions = contents.select(c => c.question).toArray()
   return questions
 }
 
-async function getQuestion(questions)
+async function getQuestionAtRandom(questions)
 {
-    var index = Math.floor( Math.random() * questions.length );
-    console.log(questions[index])
-    return questions[index]
+  const index = Math.floor(Math.random() * questions.length)
+  return questions[index]
+}
+
+async function convertEmoticonQuestion(question, answer)
+{
+  const emoticonQuestion = question.replace(answer, "＼(^o^)／")
+  return emoticonQuestion
 }
 
 async function getAccessToken()
@@ -90,11 +93,14 @@ async function getAccessToken()
   }
 
   const tokenResult = await axios.post(ACCESS_TOKEN_PUBLISH_URL, tokenDatas, tokenHeaders)
-  return tokenResult.data.access_token;
+  const accessToken = tokenResult.data.access_token
+  return accessToken
 }
 
-async function getKeyword(document, token, keywordNum = 1)
+async function getKeyword(document, keywordNum = 1)
 {
+  const token = await getAccessToken()
+
   const parseHeaders = {
     headers:{
       "Content-Type": "application/json",
@@ -108,7 +114,8 @@ async function getKeyword(document, token, keywordNum = 1)
   }
 
   const parseResult = await axios.post(DEVELOPER_API_KEYWORD_URL, parseDatas, parseHeaders)
-  return parseResult.data.result[0].form
+  const keyword = parseResult.data.result[0].form
+  return keyword
 }
 
 </script>
